096b05e790980686a72060945c1d4f49519a2f29
yuutarou <yuutarouisyokaniseboy1@gmail.com>
2020-06-11 00:11:28 +0900

    6/10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '07ef577d8f3d46f262191a416f1ea9676c9e5a9dbf6f1f137f51168efdcce34d4b992c6c1ab1ccd34e020825d52f0288c9bf00fa95f2bf00ceaded5bac0a2073'
